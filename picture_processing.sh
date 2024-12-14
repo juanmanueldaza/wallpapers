0b2a220243f8193ee7d5580c495b49946f65d4a2
Juan Manuel Daza <juandaza@camba.coop>
2024-12-13 21:50:42 -0300

    image folder replaced

--- picture_processing.sh
+++ picture_processing.sh
@@ -2,8 +2,8 @@
 
 for file in dist/pictures/*.jpg; do
     base="${file##dist/pictures/}"  #Extract filename, removing path prefix
-    convert "$file" -resize "640x>" "dist/pictures/${base}-small.webp"
-    convert "$file" -resize "1024x>" "dist/pictures/${base}-medium.webp"
-    convert "$file" -resize "1920x>" "dist/pictures/${base}-large.webp"
+    convert "$file" -resize "640x>" "dist/assets/pictures/${base}-small.webp"
+    convert "$file" -resize "1024x>" "dist/assets/pictures/${base}-medium.webp"
+    convert "$file" -resize "1920x>" "dist/assets/pictures/${base}-large.webp"
     cp "$file" "dist/pictures/${base}-xlarge.jpg"
 done
