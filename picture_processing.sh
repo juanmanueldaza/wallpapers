a4115c60c3851aeaa679007a5e13a30c6bf8a20d
Juan Manuel Daza <juandaza@camba.coop>
2024-12-13 22:25:03 -0300

    deploy corrected

--- picture_processing.sh
+++ picture_processing.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Process each image individually (most robust)
 find dist/pictures -name "*.jpg" -print0 | while IFS= read -r -d $'\0' file; do
     base="${file##dist/pictures/}"
+    #Error Handling
     if convert "$file" -resize 640x "${file%.*}-small.webp"; then
         echo "Conversion to small WebP successful: $file"
     else
-        echo "Error converting to small WebP: $file" >&2 #Send error to stderr
+        echo "Error converting to small WebP: $file" >&2
     fi
-    cp "$file" "${file%.*}-xlarge.jpg"
+    if convert "$file" -resize 1024x "${file%.*}-medium.webp"; then
+        echo "Conversion to medium WebP successful: $file"
+    else
+        echo "Error converting to medium WebP: $file" >&2
+    fi
+    #No need to cp the file again, as that's already in dist
 done
